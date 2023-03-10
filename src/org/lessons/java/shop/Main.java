0afc29c2b20380cd3bae6d2282d928a12a487973
mattialicausi <licausivitomattia@gmail.com>
2023-03-10 15:17:52 +0100

    completato consegna Java Shop con bonus

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -3,5 +3,31 @@ package org.lessons.java.shop;
 public class Main {
     public static void main(String[] args) {
 
+        //creo un instanza della classe Product usando il costruttore
+        Product p1 = new Product("Latte", "biologico", 1.99, 40 );
+
+        //stampo nel terminale i campi
+        System.out.println(p1.getName() + " " + p1.getDescription() + " " + p1.getPrice() +  " " + p1.getIva() + " " + p1.getCode());
+
+        //provo a modificare i campi
+        p1.setName("Biscotti");
+        p1.setDescription(" al cioccolato");
+        p1.setPrice(2.50);
+        p1.setIva(22);
+
+        //stampo nel terminale il prodotto con i valori diversi
+        System.out.println(p1.getName() + " " + p1.getDescription() + " " + p1.getPrice() +  " " + p1.getIva() + " " + p1.getCode());
+
+        //stampo prezzo senza iva
+        System.out.println("Prezzo base: " + p1.basePrice());
+
+        //stampo prezzo con iva
+        System.out.println("Prezzo con iva: " + p1.ivaPrice());
+
+        //stampo nome esteso
+        System.out.println("Nome esteso: " + p1.extendedName());
+
+        //stampo il codice con il pad left
+        System.out.println("Codice con pad left: " + p1.padLeft());
     }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,4 +1,108 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Product {
+
+    //campi
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private double iva;
+
+
+    //costruttori
+
+    public Product(String name, String description, double price, double iva) {
+
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+
+        Random rand = new Random();
+        this.code = rand.nextInt(1, 1000);
+
+    }
+
+
+    //getter e setter
+
+    //code getter
+    public int getCode() {
+        return code;
+    }
+
+    //name getter e setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //description getter e setter
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //price getter e setter
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //iva getter e setter
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+
+    //metodi
+
+    //prezzo base
+    public double basePrice() {
+        return price;
+    }
+
+    //prezzo con iva
+    public double ivaPrice() {
+       double iva_price = ((price * iva) / 100);
+       return price + iva_price;
+    }
+
+    //nome esteso del prodotto
+    public String extendedName() {
+        return code + "-" + name;
+    }
+
+    //padLeft
+    public String padLeft() {
+        // controlla se il codice è minore di 8 caratteri
+        if (String.valueOf(code).length() < 8) {
+            // aggiungi gli zeri necessari all'inizio del codice
+            String paddedCode = String.format("%0" + (8 - String.valueOf(code).length()) + "d%s", 0, code);
+            return paddedCode;
+        }
+        // se il codice è già di almeno 8 caratteri, restituisci il codice stesso
+        return String.valueOf(code);
+    }
+
+
 }
